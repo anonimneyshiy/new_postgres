a75f876b7860dd98812e72fd68f9adb525aeab3a
= <=>
2022-04-04 09:43:08 +0300

    added migrations

--- database/models.py
+++ database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer, Text, DateTime
+from sqlalchemy import Column, String, Integer, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 from database.utils_for_db import Base
 
@@ -8,6 +9,20 @@ class Post(Base):
 
     id = Column(Integer, primary_key=True, index=True, unique=True)
     title = Column(String)
-    text = Column(Text(350))
+    text = Column(String(length=350))
     date = Column(DateTime)
+    user = Column(Integer, ForeignKey('blog_users.id'))
+    user_id = relationship('User')
+
+
+class User(Base):
+    __tablename__ = "blog_users"
+
+    id = Column(Integer, primary_key=True, index=True, unique=True)
+    name = Column(String, unique=True)
+    email = Column(String, unique=True)
+    password = Column(String)
+    date = Column(DateTime)
+    is_active = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
 
